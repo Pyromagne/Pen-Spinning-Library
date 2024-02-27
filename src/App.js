0a3c1f90845adf8104523c64a7faa9dacddeac5b
Pyromagne <growganoth826@gmail.com>
2024-02-27 23:35:33 +0800

    Route test 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Tutorial from './pages/Tutorial';
 import TrickList from './pages/TrickList';
@@ -12,40 +12,42 @@ import pass from './data/tricks/passesData';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/tutorial" element={<Tutorial />}/>
-      <Route path="/tricks" element={<TrickMenu />}/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/tutorial" element={<Tutorial />}/>
+        <Route path="/tricks" element={<TrickMenu />}/>
 
-      {Data.trickCategoryL.map((cat, index) => (
-        <Route path={`tricks/${cat}`} key={index} element={<TrickList cat={cat}/>}/>
-      ))}
+        {Data.trickCategoryL.map((cat, index) => (
+          <Route path={`tricks/${cat}`} key={index} element={<TrickList cat={cat}/>}/>
+        ))}
 
-      {/* 
-        //TODO
-        in this code the map function generate hidden path for the 'cat' in tricks data which is the first index of the data, it must be fix
-      */}
+        {/* 
+          //TODO
+          in this code the map function generate hidden path for the 'cat' in tricks data which is the first index of the data, it must be fix
+        */}
 
-      {sonic.map((trick, index) => (
-        <Route path={`/tutorial/${trick.name}`} key={index} element={<TrickTutorial trick={trick}/>}/>
-      ))}
+        {sonic.map((trick, index) => (
+          <Route path={`/tutorial/${trick.name}`} key={index} element={<TrickTutorial trick={trick}/>}/>
+        ))}
 
-      {charge.map((trick, index) =>(
-        <Route path={`/tutorial/${trick.name}`} key={index} element={<TrickTutorial trick={trick}/>}/>
-      ))}
+        {charge.map((trick, index) =>(
+          <Route path={`/tutorial/${trick.name}`} key={index} element={<TrickTutorial trick={trick}/>}/>
+        ))}
 
-      {pass.map((trick, index) =>(
-        <Route path={`/tutorial/${trick.name}`} key={index} element={<TrickTutorial trick={trick}/>}/>
-      ))}
+        {pass.map((trick, index) =>(
+          <Route path={`/tutorial/${trick.name}`} key={index} element={<TrickTutorial trick={trick}/>}/>
+        ))}
 
-      {/*
-        //TODO END 
-      */}
+        {/*
+          //TODO END 
+        */}
 
-      <Route path="*" element={<Home />}/>
+        <Route path="*" element={<Home />}/>
 
-    </Routes>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -13,10 +13,6 @@ import '@fontsource/roboto/700.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-        <Routes>
-          <Route path='/*' element={<App />} />
-        </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
